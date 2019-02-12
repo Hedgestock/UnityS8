87c584df506d52638b82d49bfb56061d520083ee
Hedgestock <pamarthur@free.fr>
2019-02-12 13:01:59 +0100

    added collect on click

---
+++ Assets/Scripts/PickUpCollectibles.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCollectibles : MonoBehaviour
+{
+
+    public Camera playerCam;
+
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, playerCam.ScreenPointToRay(Input.mousePosition).direction * 100, Color.green);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked");
+            Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+            GameObject other = hit.collider.transform.root.gameObject;
+            Debug.Log(other);
+            if (other.layer == LayerMask.NameToLayer("Collectible"))
+            {
+                Destroy(other);
+                Debug.Log("Destroyed");
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+        GameObject other = hit.collider.gameObject;
+        if (other.layer == LayerMask.NameToLayer("Collectible"))
+        {
+            Destroy(other);
+            Debug.Log("Destroyed");
+        }
+    }
+}
