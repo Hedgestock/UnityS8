908a871583fdea0cb8eafd398635f14e86f7a78c
Sylvain Levert <sylvain.levert@epita.fr>
2019-02-14 15:16:52 +0100

    When enough car go to second scene

---
+++ Assets/Scripts/GoToSecond.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GoToSecond : MonoBehaviour
+{
+
+    public Slider carSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (carSlider.value == carSlider.maxValue)
+        {
+            SceneManager.LoadScene("Second");
+        }
+    }
+}
