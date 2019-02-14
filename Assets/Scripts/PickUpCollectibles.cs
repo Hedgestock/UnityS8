79449fdae16138f220afb0a995097032648b4be8
Hedgestock <pamarthur.aito@gmail.com>
2019-02-14 23:30:46 +0100

    Added full functionnal solar pannels

--- Assets/Scripts/PickUpCollectibles.cs
+++ Assets/Scripts/PickUpCollectibles.cs
@@ -10,15 +10,18 @@ public class PickUpCollectibles : MonoBehaviour
     public Camera playerCam;
     public Text scoreText;
     public Slider carSlider;
+    public int solarScore = 3;
 
     private RaycastHit hit;
     private int score = 0;
+    private int solarNum = 0;
     private int carScore = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine("SolarTimer");
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
@@ -36,17 +39,37 @@ public class PickUpCollectibles : MonoBehaviour
                     score_up(other);
                     Destroy(other);
                 }
+                if (other.layer == LayerMask.NameToLayer("SolarPanel"))
+                {
+                    if (other.GetComponent<SolarPanel>().build(ref score))
+                    {
+                        solarNum++;
+                    }
+                }
             }
         }
+        scoreText.text = "Score = " + score;
     }
 
     void score_up(GameObject other)
     {
         score += ((Trash) other.GetComponent(typeof(Trash))).Value;
-        scoreText.text = "Score = " + score;
         if (other.tag == "Car_Trash")
         {
             carSlider.value++;
+        }
+    }
+
+    IEnumerator SolarTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            Debug.Log(solarNum);
+            Debug.Log(solarScore);
+            Debug.Log(score);
+            score += solarNum * solarScore;
+            Debug.Log(score);
         }
     }
 }
---
+++ Assets/Scripts/SolarPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarPanel : MonoBehaviour
+{
+
+    public int cost = 15;
+    public GameObject placeHolder;
+    public GameObject panel;
+    bool isActive = true;
+
+    public bool build(ref int score)
+    {
+        if (score >= cost)
+        {
+            score -= cost;
+            placeHolder.SetActive(false);
+            panel.SetActive(true);
+            isActive = false;
+            return true;
+        }
+        return false;
+    }
+}
